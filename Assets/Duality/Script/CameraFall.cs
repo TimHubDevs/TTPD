c9cf91adabd0a1726df3ce4406cfa9a357d95cfa
denshust <denshust@ukr.net>
2022-01-24 22:05:14 +0200

    first
    1

---
+++ Assets/Duality/Script/CameraFall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFall : MonoBehaviour
+{
+    float h = 0;
+    public float koeff=10;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, transform.position.y-h*koeff, transform.position.z);
+    }
+}
---
+++ Assets/Duality/Script/Platform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public int[] indexsesToDrop;
+    public bool isRandom;
+    void Start()
+    {
+        
+        foreach (int index in indexsesToDrop)
+        {
+            if (isRandom)
+            {
+                int newIndex = Random.Range(1, 10);
+                Destroy(transform.GetChild(newIndex - 1).gameObject);
+            }
+            else
+                Destroy(transform.GetChild(index - 1).gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
