4f6f12af87c9f8b3add4feac423a1bb599e1c1b3
Tim <devsspacegame@gmail.com>
2022-01-30 16:15:52 +0200

    New Animation Player

---
+++ Assets/Duality/Script/Projectile/Projectile.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private Rigidbody2D rigidbody;
+    [SerializeField] private float _speed = 0;
+
+    public void SetSpeed(float speed)
+    {
+        _speed = speed;
+    }
+
+    private void Update()
+    {
+        var moveBy = 1 * _speed;
+        rigidbody.velocity = new Vector2(moveBy, rigidbody.velocity.y);
+    }
+
+    public void Setup()
+    {
+        // animator.SetBool("isJumping", true);
+    }
+}
