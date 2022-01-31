c9921badf9455d05e8b710066abed26b93d296f9
Tim <devsspacegame@gmail.com>
2022-01-31 20:49:29 +0200

    fix

--- Assets/Duality/Script/Platform/TiledPlatform.cs
+++ Assets/Duality/Script/Platform/TiledPlatform.cs
@@ -6,35 +6,42 @@ public class TiledPlatform : MonoBehaviour
 {
     void Start()
     {
-       
     }
 
-    public void SpawnTiles(GameObject tilePrefab, int[] tilesToDelete, Sprite[] tileVars, GameObject[] enemyPrefs, int[] tilesToSpawnWhite, int[] tilesToSpawnBlack)
+    public void SpawnTiles(GameObject tilePrefab, int[] tilesToDelete, Sprite[] tileVars, GameObject[] enemyPrefs,
+        int[] tilesToSpawnWhite, int[] tilesToSpawnBlack)
     {
         for (int i = 0; i < 18; i++)
         {
-            GameObject tile = Instantiate(tilePrefab, transform.position + new Vector3(i * 2, 0, 0), Quaternion.identity);
+            GameObject tile = Instantiate(tilePrefab, transform.position + new Vector3(i * 2, 0, 0),
+                Quaternion.identity);
             tile.transform.SetParent(transform);
             tile.GetComponent<SpriteRenderer>().sprite = tileVars[Random.Range(0, tileVars.Length)];
-           
+
             foreach (var item in tilesToSpawnWhite)
             {
-                if(item==i&&tile.tag!="Spawned")
+                if (item == i && tile.tag != "Spawned")
                 {
-                    GameObject white = Instantiate(enemyPrefs[Random.Range(enemyPrefs.Length / 2, enemyPrefs.Length)], tile.transform.position + new Vector3(0, 2.3f, 0), Quaternion.identity);
+                    if (enemyPrefs.Length <= 0) continue;
+                    GameObject white = Instantiate(enemyPrefs[Random.Range(enemyPrefs.Length / 2, enemyPrefs.Length)],
+                        tile.transform.position + new Vector3(0, 2.3f, 0), Quaternion.identity);
                     white.transform.SetParent(tile.transform);
                     tile.tag = "Spawned";
                 }
             }
+
             foreach (var item in tilesToSpawnBlack)
             {
                 if (item == i && tile.tag != "Spawned")
                 {
-                    GameObject black = Instantiate(enemyPrefs[Random.Range(0, enemyPrefs.Length / 2)], tile.transform.position - new Vector3(0, 2.3f, 0), Quaternion.Euler(0, 0, 180));
+                    if (enemyPrefs.Length <= 0) continue;
+                    GameObject black = Instantiate(enemyPrefs[Random.Range(0, enemyPrefs.Length / 2)],
+                        tile.transform.position - new Vector3(0, 2.3f, 0), Quaternion.Euler(0, 0, 180));
                     black.transform.SetParent(tile.transform);
                     tile.tag = "Spawned";
                 }
             }
+
             foreach (var item in tilesToDelete)
             {
                 if (item == i)
@@ -49,6 +56,5 @@ public class TiledPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
