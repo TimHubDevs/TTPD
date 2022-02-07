f4bc8225b2e05498aa665fe4b62d9c05b26fd5a5
denshust <denshust@ukr.net>
2022-02-07 15:18:23 +0200

    Combat & Grav
    Combat system fixed a bit
    Gravity change added AGAIN :D

--- Assets/Duality/Script/Player/StaffAttacker.cs
+++ Assets/Duality/Script/Player/StaffAttacker.cs
@@ -4,11 +4,11 @@ public class StaffAttacker : MonoBehaviour
 {
     [SerializeField] private float _damageAmount = 25f;
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Enemy")
         {
-            Debug.Log("POOF");
+            Debug.Log("CloseAttackOnEnemy");
             if (other.gameObject.GetComponent<Health>() == null)
             {
                 Debug.LogError($"This {other.gameObject.name} doesn't have Health script!");
--- Assets/Duality/Script/Projectile/Projectile.cs
+++ Assets/Duality/Script/Projectile/Projectile.cs
@@ -6,26 +6,39 @@ public class Projectile : MonoBehaviour
     [SerializeField] private Rigidbody2D _rigidbodyProjectile;
     private float _speed = 0;
     [SerializeField] private float _damageAmount = 25f;
-
+    Vector3 shootDirection;
     public void SetSpeed(float speed)
     {
-        _speed = speed;
+        if (_speed == 0)
+        {
+            Destroy(gameObject, 1);
+            _speed = speed;
+            shootDirection = Input.mousePosition;
+            shootDirection.z = 0.0f;
+            shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+            shootDirection = shootDirection - transform.position;
+            shootDirection = shootDirection.normalized;
+        }
     }
 
     private void Update()
     {
         var moveBy = 1 * _speed;
-        _rigidbodyProjectile.velocity = new Vector2(moveBy, _rigidbodyProjectile.velocity.y);
+        _rigidbodyProjectile.velocity = new Vector2(shootDirection.x * moveBy, shootDirection.y * moveBy);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Enemy")
         {
-            Debug.Log("PAM");
+            Debug.Log("Projectile collided with Enemy");
             other.gameObject.GetComponent<Health>().DecreaseHealth(_damageAmount);
             _rigidbodyProjectile.gravityScale = 0;
             _animator.SetTrigger("isBoom");
+        }
+        else if (other.tag == "Platform")
+        {
+            KillProjectile();
         }
     }
 
