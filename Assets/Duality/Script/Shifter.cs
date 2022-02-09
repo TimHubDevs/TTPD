283682209a549afcc6f3553cbe786e7d21e44cf3
denshust <denshust@ukr.net>
2022-02-09 14:29:12 +0200

    Portal added

--- Assets/Duality/Script/Shifter.cs
+++ Assets/Duality/Script/Shifter.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using System.Collections;
+using System;
 
 public class Shifter : MonoBehaviour
 {
     [SerializeField] GameObject darkPlayMode;
     [SerializeField] GameObject lightPlayMode;
     [SerializeField] GameObject remains;
+    [SerializeField] GameObject shiftScreen;
+    bool shifted;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (shifted) return;
+        shifted = true;
         if (!collision.CompareTag("Player")) return;
-        lightPlayMode.SetActive(false);
+        shiftScreen.GetComponent<Animator>().SetBool("shift", true);
+        //collision.GetComponent<PlayerController>()._isShifting = true;
+    }
+
+    public void Shift()
+    {
         darkPlayMode.SetActive(true);
+        shiftScreen.GetComponent<ShiftScreen>().GetAdopted();
+        lightPlayMode.SetActive(false);
         remains.SetActive(true);
         STATIC.ISBLACK = !STATIC.ISBLACK;
-        Destroy(gameObject);
+        GetComponent<SpriteRenderer>().enabled = false;
+       // Destroy(gameObject,3);
     }
 }
